c2df80248209c3c280754baf3445178a13f3d943
Paul McQuade <paulmcquad@gmail.com>
2020-03-11 19:41:35 +0000

    Created a about page
    Added my settings to the page
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Portfolio
+
 My Portfolio Page - VueJS &amp; NuxtJS Framework Project
+
+> List of Projects
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is an about page</p>
+
+    <p>
+      <li>Project name Portfolio</li>
+      <li>Project description List of Projects</li>
+      <li>Author name Paul McQuade</li>
+      <li>Choose the package manager Npm</li>
+      <li>Choose UI framework None</li>
+      <li>Choose custom server framework None (Recommended)</li>
+      <li>Choose Nuxt.js modules</li>
+      <li>Choose linting tools</li>
+      <li>Choose test framework None</li>
+      <li>Choose rendering mode Universal (SSR)</li>
+      <li>Choose development tools</li>
+      <li>Choose test framework None</li>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
