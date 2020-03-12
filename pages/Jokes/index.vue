aea99e27a569c83854c42068f2c05309abe89427
Paul McQuade <paulmcquad@gmail.com>
2020-03-12 20:42:17 +0000

    Completed Joke WebApp
    Added a SearchJokes Function
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes v-on:search-text="searchText" />
     <Joke
       v-for="joke in jokes"
       :key="joke.id"
@@ -12,12 +13,13 @@
 <script>
 import axios from "axios";
 import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
 
 export default {
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
-
   data() {
     return {
       jokes: []
@@ -32,14 +34,32 @@ export default {
 
     try {
       const res = await axios.get("https://icanhazdadjoke.com/search", config);
-      this.jokes = res.data.results;
 
-      // console.log(res.data);
+      this.jokes = res.data.results;
     } catch (err) {
       console.log(err);
     }
   },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
 
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
   head() {
     return {
       title: "Dad Jokes",
