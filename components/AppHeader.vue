e004dc1be49abf5ef2015173c462b32d4a3400b6
Paul McQuade <paulmcquad@gmail.com>
2020-03-13 15:47:15 +0000

    Vue Navbar
    Added Dropdown Menu
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -11,7 +11,23 @@
       <li>
         <nuxt-link to="/about">About</nuxt-link>
       </li>
+      <li>
+        <div>
+
+        <div class="dropdown">
+        <nuxt-link to="/about">Drop</nuxt-link>
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="/bob">Link 1</a>
+            <a href="/alice">Link 2</a>
+            <a href="/sam">Link 3</a>
+          </div>
+        </div> 
+        </div>
+      </li>
     </ul>
+   </div>
   </header>
 </template>
 
@@ -44,4 +60,39 @@ export default {
   padding: 0.3rem 1rem;
   margin-right: 0.5rem;
 }
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 80px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: white;
+  padding: 12px 20px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
 </style>
